759cd01300345285e1efaa77f496a77426b6c5ff
201311185강성규 <tj5192@naver.com>
2017-05-23 15:22:21 +0900

    Update wiki.py

--- src/wiki.py
+++ src/wiki.py
@@ -1,6 +1,6 @@
 import os
 import sys 
-os.environ["SPARK_HOME"]="C:\Users\dy\Downloads\spark-2.0.0-bin-hadoop2.6\spark-2.0.0-bin-hadoop2.6"
+os.environ["SPARK_HOME"]="C:\Users\bolto\Downloads\spark-2.0.0-bin-hadoop2.6\spark-2.0.0-bin-hadoop2.6"
 os.environ["PYLIB"]=os.path.join(os.environ["SPARK_HOME"],'python','lib')
 sys.path.insert(0,os.path.join(os.environ["PYLIB"],'py4j-0.10.1-src.zip'))
 sys.path.insert(0,os.path.join(os.environ["PYLIB"],'pyspark.zip'))
