a4d99f64b4d5338c7b6603fac1396217b5336b70
201311185강성규 <tj5192@naver.com>
2017-05-24 00:24:46 +0900

    Add files via upload

---
+++ src/rdd_wordcounter.py
@@ -0,0 +1,13 @@
+import os
+import sys 
+os.environ["SPARK_HOME"]=os.path.join(home+'//'+'Downloads'+'//'+'spark-2.0.0-bin-hadoop2.6')
+os.environ["PYLIB"]=os.path.join(os.environ["SPARK_HOME"],'python','lib')
+sys.path.insert(0,os.path.join(os.environ["PYLIB"],'py4j-0.10.1-src.zip'))
+sys.path.insert(0,os.path.join(os.environ["PYLIB"],'pyspark.zip'))
+import pyspark
+
+documents = spark.sparkContext.textFile("ds_spark_wiki.txt").map(lambda line: line.split(" "))
+from pyspark.mllib.feature import HashingTF
+hashingTF = HashingTF()
+tf = hashingTF.transform(documents)
+tf.collect()
